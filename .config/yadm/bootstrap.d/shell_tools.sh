65c2d2651c764ababf01733f372fee77edd9f57b
Brad Mann <bradmann@gmail.com>
2025-01-08 11:24:24 -0500

    more changes

--- .config/yadm/bootstrap.d/shell_tools.sh
+++ .config/yadm/bootstrap.d/shell_tools.sh
@@ -4,5 +4,8 @@ system_type=$(uname -s)
 if [ "$system_type" = "Darwin" ]; then
     brew install lsd
 else
+    if ! command -v cargo &> /dev/null; then
+        curl https://sh.rustup.rs -sSf | sh
+    fi
     cargo install lsd
 fi
